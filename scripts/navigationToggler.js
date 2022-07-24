083737985c5dc2c60041db7eb30c3fa275dd867d
kazuhachi <107222643+kazuhachi@users.noreply.github.com>
2022-07-24 19:16:39 +0800

    huge chages on files. Navigation seems to work at expectation
    mini bug: when reloading the page slider doesnt remember it state whether is it collapsed or uncollapsed. Definitely the iniatiation of toogleWindowResize() on page load causes the problem

--- scripts/navigationToggler.js
+++ scripts/navigationToggler.js
@@ -1,26 +1,33 @@
+// alert()
+
+$("body left-body").addClass("shrinked");
+
 $(document).ready(function(){
     //initialize navigation state "collapsed or uncollapsed"
-
     let body = $(".right-body");
     let navigation = $(".left-body");
+    let topHeader = $(".header-container");
+    let mainContainer = $(".main-content-container");
+
+    toggleOnWindowResize();
     
-    if (localStorage.getItem("isNavigationCollapsed") === null){
-        localStorage.setItem("isNavigationCollapsed", false)
+    // if (localStorage.getItem("isNavigationCollapsed") === null){
+    //     localStorage.setItem("isNavigationCollapsed", false)
         
 
-    }else{
-        let isNavigationCollapsed = localStorage.getItem("isNavigationCollapsed");
+    // }else{
+    //     let isNavigationCollapsed = localStorage.getItem("isNavigationCollapsed");
         
-        // bool true doesnt worked. uses string instead
-        if (isNavigationCollapsed == "true"){
-            setTimeout(function(){
-                collapseNavigation();
-            },1000)
-        }else{
+    //     // bool true doesnt worked. uses string instead
+    //     if (isNavigationCollapsed == "true"){
+    //         setTimeout(function(){
+    //             collapseNavigation();
+    //         },0)
+    //     }else{
             
-            uncollapseNavigation();
-        }
-    }
+    //         uncollapseNavigation();
+    //     }
+    // }
 
 
 
@@ -35,6 +42,12 @@ $(document).ready(function(){
     })
 
     function toggleNavigation(){
+
+        let windowSize = $(window).width();
+        let isOnMobileWidth = false;
+        if (windowSize <= 800){
+            isOnMobileWidth = true;
+        }
 
 
         if (localStorage.getItem("isNavigationCollapsed") === null){
@@ -60,11 +73,21 @@ $(document).ready(function(){
     }
 
     function collapseNavigation(){
+
+        
         navigation.addClass("shrinked");
+        topHeader.addClass("widened");
+        body.addClass("widened");
     }
 
     function uncollapseNavigation(){
+        let windowWidth = $(window).width();
+        if (!(windowWidth <= 800)){
+            topHeader.removeClass("widened");
+            body.removeClass("widened");
+        }
         navigation.removeClass("shrinked");
+        
     }
 
 
@@ -72,32 +95,43 @@ $(document).ready(function(){
     // navigation hides when min width has reached
     // basically tooggles
     $(window).on("resize", function(){
+        toggleOnWindowResize();
+    })
+
+    function toggleOnWindowResize(){
         let windowWidth = $(window).width();
         console.log(windowWidth);
         if (windowWidth <= 800){
+            // make left slider 100% on width 
+            $(".left-body").addClass("is-on-mobile-width");
+
             if (localStorage.getItem("isNavigationCollapsed") === null){
                 localStorage.setItem("isNavigationCollapsed", true)
-                navigation.addClass("shrinked")
+                collapseNavigation()
             }else{
                 if (localStorage.getItem("isNavigationCollapsed") == "false"){
                     
                     localStorage.setItem("isNavigationCollapsed", true)
-                    navigation.addClass("shrinked")
+                    collapseNavigation()
                 }
             }
         }
         else{
+            // remove left slider 100% on width 
+            $(".left-body").removeClass("is-on-mobile-width");
+
+
             if (localStorage.getItem("isNavigationCollapsed") === null){
                 localStorage.setItem("isNavigationCollapsed", false)
-                navigation.removeClass("shrinked")
+                uncollapseNavigation()
             }else{
                 if (localStorage.getItem("isNavigationCollapsed") == "true"){
                     
                     localStorage.setItem("isNavigationCollapsed", false)
-                    navigation.removeClass("shrinked")
+                    uncollapseNavigation()
                 }
             }
         }
-    })
+    }
 
 })
